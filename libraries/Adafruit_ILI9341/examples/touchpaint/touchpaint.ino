0c8e09701cf2b58b8017395951806ceb2ce78aa1
Tommy <tab114@pitt.edu>
2016-11-19 15:34:06 -0500

    included library dependencies

--- libraries/Adafruit_ILI9341/examples/touchpaint/touchpaint.ino
+++ libraries/Adafruit_ILI9341/examples/touchpaint/touchpaint.ino
@@ -36,10 +36,9 @@ Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 // Size of the color selection boxes and the paintbrush size
-#define BOXSIZE 120
+#define BOXSIZE 40
 #define PENRADIUS 3
-int currentspeed;
-bool isrunning;
+int oldcolor, currentcolor;
 
 void setup(void) {
  // while (!Serial);     // used for leonardo debugging
@@ -55,17 +54,19 @@ void setup(void) {
   }
   Serial.println("Touchscreen started");
   
-  
   tft.fillScreen(ILI9341_BLACK);
   
-  drawStart();
-  drawReset();
-  drawIncrement();
-  drawDecrement();
-  drawSpeed();
-  drawCurrentSpeed(currentspeed);
+  // make the color selection boxes
+  tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
+  tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
+  tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_GREEN);
+  tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_CYAN);
+  tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_BLUE);
+  tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_MAGENTA);
  
-  setRunState(true);
+  // select the current color 'red'
+  tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+  currentcolor = ILI9341_RED;
 }
 
 
@@ -95,73 +96,51 @@ void loop()
   p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
   p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
 
-  
-//  Serial.print("("); Serial.print(p.x);
-//  Serial.print(", "); Serial.print(p.y);
-//  Serial.println(")");
-  
+  /*
+  Serial.print("("); Serial.print(p.x);
+  Serial.print(", "); Serial.print(p.y);
+  Serial.println(")");
+  */
 
   if (p.y < BOXSIZE) {
-     setRunState(p.x < BOXSIZE);
-  } else if (p.y < 2*BOXSIZE) {
+     oldcolor = currentcolor;
+
      if (p.x < BOXSIZE) { 
-       currentspeed++;
-     } else {
-       currentspeed--;
+       currentcolor = ILI9341_RED; 
+       tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*2) {
+       currentcolor = ILI9341_YELLOW;
+       tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*3) {
+       currentcolor = ILI9341_GREEN;
+       tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*4) {
+       currentcolor = ILI9341_CYAN;
+       tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*5) {
+       currentcolor = ILI9341_BLUE;
+       tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*6) {
+       currentcolor = ILI9341_MAGENTA;
+       tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
      }
-     
-     drawCurrentSpeed(currentspeed);
+
+     if (oldcolor != currentcolor) {
+        if (oldcolor == ILI9341_RED) 
+          tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
+        if (oldcolor == ILI9341_YELLOW) 
+          tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
+        if (oldcolor == ILI9341_GREEN) 
+          tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_GREEN);
+        if (oldcolor == ILI9341_CYAN) 
+          tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_CYAN);
+        if (oldcolor == ILI9341_BLUE) 
+          tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_BLUE);
+        if (oldcolor == ILI9341_MAGENTA) 
+          tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_MAGENTA);
+     }
   }
-  
-}
-
-void setRunState(bool runstate) {
-  if (isrunning == runstate) return;
-  isrunning = runstate;
-  if (runstate) {
-    tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-    drawReset();
-  } else {
-    tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-    drawStart();
+  if (((p.y-PENRADIUS) > BOXSIZE) && ((p.y+PENRADIUS) < tft.height())) {
+    tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
   }
-}
-
-void drawStart() {
-    tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_BLUE);
-    tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(3);
-    tft.setCursor(0.1*BOXSIZE, 0.4*BOXSIZE);
-    tft.println("Start");
-}
-
-void drawReset() {
-    tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
-    tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(3);
-    tft.setCursor(1.1*BOXSIZE, 0.4*BOXSIZE);
-    tft.println("Reset");
-}
-
-void drawIncrement() {
-  tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(8);
-  tft.setCursor(0.35*BOXSIZE, 1.4*BOXSIZE);
-  tft.println("+");
-}
-
-void drawDecrement() {
-  tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(8);
-  tft.setCursor(1.35*BOXSIZE, 1.4*BOXSIZE);
-  tft.println("-");
-}
-
-void drawSpeed() {
-  tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(3);
-  tft.setCursor(0.1*BOXSIZE, 2.4*BOXSIZE);
-  tft.println("Speed");
-}
-
-void drawCurrentSpeed(int currentspeed) {
-     tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(3);
-     tft.fillRect(BOXSIZE, 2*BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_BLACK);
-     tft.setCursor(1.35*BOXSIZE, 2.4*BOXSIZE);
-     tft.println(currentspeed);
 }
